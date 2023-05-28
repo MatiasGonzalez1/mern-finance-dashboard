e987bccef2ea1e235af2f2be36cd46f3255e334a
Matias Gonzalez <mng_epson@hotmail.com>
2023-05-28 09:02:53 -0300

    [feat: add styles to the first graph]

--- mern-finance/src/scenes/dashboard/Row1.tsx
+++ mern-finance/src/scenes/dashboard/Row1.tsx
@@ -2,51 +2,87 @@ import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
 import { useMemo } from "react";
-import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Area, Tooltip} from "recharts";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  // CartesianGrid,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+} from "recharts";
 
 type Props = {};
 
 const Row1 = (props: Props) => {
-
-  const {palette} = useTheme();
-  const {data} = useGetKpisQuery();
-  console.log('data:', data)
-  const revenueExpenses = useMemo(()=>{
+  const { palette } = useTheme();
+  const { data } = useGetKpisQuery();
+  console.log("data:", data);
+  const revenueExpenses = useMemo(() => {
     return (
-      data && 
-      data[0].monthlyData.map(({month, revenue, expenses})=>{
-        return{
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
           // toma los primeros tres caracteres de la palabra
           name: month.substring(0, 3),
           revenue: revenue,
           expenses: expenses,
-        }
+        };
       })
-    )
-  }, [data])
+    );
+  }, [data]);
   return (
     <>
       <DashboardBox gridArea={"a"}>
-        <ResponsiveContainer width='100%' height= "100%">
+        <ResponsiveContainer width="100%" height="100%">
+          {/*manejo de estilos de la grafica*/}
           <AreaChart
-          width={500}
-          height={400}
-          data={revenueExpenses}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{
+              top: 15,
+              right: 25,
+              left: -10,
+              bottom: 0,
+            }}
           >
-            <CartesianGrid strokeDasharray="3 3"/>
-            <XAxis dataKey="name"/>
-            <YAxis/>
-            <Tooltip/>
-            <Area type="monotone" dataKey="revenue" 
-            stroke={palette.primary.main} 
-            fillOpacity={1}
-            fill="url(#colorRevenue)"/>
+            
+            {
+            /*
+            Esta linea muestra el eje cartesiano
+            <CartesianGrid strokeDasharray="3 3" /> 
+            */}
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+            <XAxis dataKey="name" tickLine={false} style={{fontSize:"10px"}}
+            />
+            <YAxis axisLine={{strokeWidth: "0"}} tickLine={false} style={{fontSize:"10px"}} domain={[8000, 23000]}/>
+            <Tooltip />
+            <Area
+              type="monotone"
+              dot={true}
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dot={true}
+              dataKey="expenses"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
+            />
           </AreaChart>
         </ResponsiveContainer>
       </DashboardBox>
